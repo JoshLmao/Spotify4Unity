c8a104826d790537436916173ad065ae96e5fdbe
Josh Shepherd <josh_lmao@live.co.uk>
2021-06-14 11:19:33 +0100

    Init new project, SpotifyAPI v6.1.0

---
+++ Spotify4Unity/Assets/Spotify4Unity/Core/SpotifyService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SpotifyAPI.Web;
+
+public class SpotifyService : Singleton<SpotifyService>
+{
+    // Spotify Dashboard client id
+    public string SPOTIFY_CLIENT_ID = "8c42bb0a9bd8483986929038af40ed4a";
+
+    public SpotifyService()
+    {
+        StartService();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private async void StartService()
+    {
+        var client = new SpotifyClient(SPOTIFY_CLIENT_ID);
+
+        var me = await client.UserProfile.Current();
+        Debug.Log(me.DisplayName);
+    }
+}
--- Spotify4Unity/ReadMe.md
+++
@@ -1,5 +0,0 @@
-# Source Code
-
-Since 15/01/2020, Spotify4Unity is now open source. I originally developed it closed source and hosted the code on Bitbucket and am continuing to develop using the Bitbucket repository. If you wish to see the source online, visit the Bitbucket repo [here](https://bitbucket.org/JoshLmao/spotify4unity) or using the link below 
-
-https://bitbucket.org/JoshLmao/spotify4unity

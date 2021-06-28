dc48ceb2d0ed713b10c4319d1a75dbdf5c16dac6
Josh Shepherd <josh_lmao@live.co.uk>
2021-06-28 17:32:40 +0100

    Upgraded bat file to build Wiki

--- tools/wiki-tool/GithubWiki2HTML.bat
+++ tools/wiki-tool/GithubWiki2HTML.bat
@@ -1,7 +1,20 @@
-// Pull git repo
+@echo off
+
+echo Updating Wiki repository
+
+rem Step into folder
 cd ".\Spotify4Unity.wiki"
-git pull
-// Go up a folder
+
+rem Pull git repo fore latest wiki
+git submodule update --init --recursive
+
+rem Go up a folder
 cd ".."
-// Convert wiki to html
-gwtc ".\Spotify4Unity.wiki" --file-name "Spotify4Unity_Documentation"
+
+rem Ask for version name
+set /p version="Enter Version: "
+
+echo Running GWTC to create Wiki file
+
+rem Convert wiki to html
+gwtc ".\Spotify4Unity.wiki" --file-name ".\Spotify4Unity.wiki\Spotify4Unity_v%version%_Documentation"
